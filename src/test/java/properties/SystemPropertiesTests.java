bf4953540e0a94c1b86f010ad8e83eb3d28740fb
Ilya_Vezhnovets <il.vezhnovets@gmail.com>
2024-06-26 21:49:56 +0700

    add beforeeach

--- src/test/java/properties/SystemPropertiesTests.java
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -1,11 +1,18 @@
 package properties;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
 public class SystemPropertiesTests {
 
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
     @Test
     void simplePropertyTest() {
         System.setProperty("browser", "opera");
